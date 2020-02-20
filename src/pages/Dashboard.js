556aa7e971c4632821077fae98e5bef4008bbcbf
Vitor Boccio <vitorboccio@gmail.com>
2020-02-20 18:33:22 +1100

    configuring semantic ui react

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
 
 const Dashboard = () => {
   return (
     <div>
       Dashboard
+      <Button>abc</Button>
     </div>
   )
 }
