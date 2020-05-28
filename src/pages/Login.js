abc1e229d2853edf45e8fe4de129455ae854c5dc
Vitor Boccio <vitorboccio@gmail.com>
2020-05-29 09:41:00 +1000

    loader

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Grid, Segment } from 'semantic-ui-react';
 import GoogleLogin from 'react-google-login';
 import { GraphQLClient } from 'graphql-request';
@@ -7,30 +7,34 @@ import Context from '../context';
 import { ME_QUERY } from '../graphql/queries';
 
 const Login = () => {
-  const { dispatch } = useContext(Context);
+  const { dispatch, state: { isAuth } } = useContext(Context);
+  const [loading, setLoading] = useState(false);
   const onSuccess = async googleUser => {
+    setLoading(true);
     try {
       const tokenId = googleUser.getAuthResponse().id_token;
       const client = new GraphQLClient(BASE_URL, {
         headers: { authorization: tokenId }
       })
-      console.log('await');
       const { me } = await client.request(ME_QUERY);
-      console.log({ me });
       dispatch({ type: "LOGIN_USER", payload: me });
       dispatch({ type: "IS_LOGGED_IN", payload: googleUser.isSignedIn() })
     } catch (err) {
       console.log({ err });
       dispatch({ type: "IS_LOGGED_IN", payload: false });
+    } finally {
+      setLoading(false);
     }
   }
+  console.log({ isAuth })
   return (
     <>
       <Grid centered>
         <Grid.Column width={10}>
           <Segment>
           <GoogleLogin
-            buttonText="Login with Google"
+            buttonText={!loading ? "Login with Google" : 'Loading...'}
+            disabled={loading}
             clientId="425993808005-tq26jpnsupbrupqvtcjn8es2nl3qkc95.apps.googleusercontent.com"
             onSuccess={onSuccess}
             onFailure={() => console.log('failure')}
